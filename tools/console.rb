57cda428a8fc5b516c74c874b9aa8e899ecb974d
bellex0 <github email address>
2019-08-30 14:31:55 -0400

    done.

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,12 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+apple = Startup.new("apple", "Belle", "domain1")
+etsy = Startup.new("esty", "Jess", "domain2")
+
+vc1 = VentureCapitalist.new("George", 3000000000)
+vc2 = VentureCapitalist.new("Anna", 900000)
+fr1 = FundingRound.new(apple, vc1, "Angel", "3 million")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
