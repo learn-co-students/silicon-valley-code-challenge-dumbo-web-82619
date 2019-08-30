88befe7333c7072d181bf6f265696b14e4441a6f
github username <github email address>
2019-08-30 14:28:23 -0400

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,25 @@
 class FundingRound
 
+    attr_reader(:startup, :venture_capitalist, :investment, :type)
+
+    @@all = []
+
+    def initialize(type, investment, startup, venture_capitalist)
+        if investment < 0
+            type = type
+            @investment = "Please give a proper investment"
+            @startup = startup
+            @venture_capitalist = venture_capitalist
+        else
+            @type = type
+            @investment = investment.to_s.to_f.round(2)
+            @startup = startup
+            @venture_capitalist = venture_capitalist
+            @@all << self
+        end
+    end
+
+    def self.all
+        @@all
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,23 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+bennyland = Startup.new("Benny Land", "Benny Louie", "Entertainment")
+bennytopia = Startup.new("BennyTopia", "Benny Louie", "City")
+newheaven = Startup.new("New Heaven", "Random Deity", "World")
+newhell = Startup.new("New Hell", "Random Deity", "World")
+
+geniuslookout = VentureCapitalist.new("Genius Lookout", 5000000000)
+noviceSeeker = VentureCapitalist.new("Novice Seeker", 850000)
+fruitofheaven = VentureCapitalist.new("Fruit of Heaven", 2500000000)
+poisonfruit = VentureCapitalist.new("Poison Fruit", 4255000000)
+
+f1 = FundingRound.new("Pre-Seed", 80000, bennyland, noviceSeeker)
+f2 = FundingRound.new("Seed", 200000, newheaven, geniuslookout)
+f3 = FundingRound.new("Seed", 5000, newhell, noviceSeeker)
+f4 = FundingRound.new("Pre-Seed", 1000000000, bennytopia, geniuslookout)
+f5 = FundingRound.new("Seed", 364000, bennyland, geniuslookout)
+f6 = FundingRound.new("Pre-Seed", 100000, bennyland, fruitofheaven)
+f7 = FundingRound.new("Seed", 85000, bennyland, poisonfruit)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
