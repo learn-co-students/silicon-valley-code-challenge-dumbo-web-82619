b3cc3b53421760974a4507c60b7b336fc2bc11e4
reireynoso <reireynoso@gmail.com>
2019-08-31 14:33:49 -0400

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,77 @@
 class VentureCapitalist
+    attr_reader :total_worth, :name
+    @@all = []
+    def initialize(name, total_worth)
 
+        @name = name
+        @total_worth = total_worth
+
+        @@all << self
+
+    end
+
+    def offer_contract(start_up_obj, type, amount)
+        FundingRound.new(start_up_obj,self, type, amount)
+    end
+
+    def funding_rounds 
+        FundingRound.all.select do |fr|
+            fr.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        funding_rounds.map do |fr|
+            # binding.pry
+            fr.startup.name
+        end.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max_by do |fr|
+            fr.investment
+        end
+    end
+
+    def invested(domain_string)
+        domain = funding_rounds.select do |fr|
+            fr.startup.domain == domain_string
+            # binding.pry
+        end
+        domain.sum do |fr|
+            # binding.pry
+            fr.investment
+        end
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def self.tres_commas_club
+        # @@all.select do |vc|
+        #     self.commas(vc.total_worth) >= 3
+        # end
+        @@all.select do |vc| 
+            vc.total_worth > 1000000000
+        end
+    end
+
+    # def self.commas(x)
+    #     formatted_number = []
+    #     index = 0
+    #     length = x.to_s.reverse.split("").size
+    #     x.to_s.reverse.split("").map do |letter|
+    #         if index == 2 
+    #             index = 0
+    #             # formatted_number.push(letter)
+    #             formatted_number.push(",")
+    #         else
+    #             # formatted_number.push(letter)
+    #             index += 1 
+    #         end  
+    #     end
+    #     formatted_number.size
+    #     # formatted_number.reverse.join("")
+    #   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup1 = Startup.new("McDo", "Ronald", "Global")
+startup2 = Startup.new("Jobee", "Bee", "Philippines")
+
+musk = VentureCapitalist.new("Elon Musk", 1000000000)
+gates = VentureCapitalist.new("Bill Gates", 2000000000)
+rey = VentureCapitalist.new("Rei Rey", 20000)
+
+FundingRound.new(startup1,musk,"Angel", 2000)
+FundingRound.new(startup1,musk,"Angel", 4000)
+FundingRound.new(startup1, gates, "SuperAngel", 5000)
+
+FundingRound.new(startup2,musk, "Devil", 1000)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
